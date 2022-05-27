2e12a41926dd513ea309d9b6a7bb0b1bdb6e4104
Son Nguyen <nam_son14@yahoo.com>
2022-05-27 22:18:08 +1000

    remove uneed files

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"=<k!zY3x<>buqUAe<m>E#3W~R:QwHLc@(o?g%r/?yW,B<]kBz,Rk!iTP}/9hSOwY";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:635;}
